b25ea3b79488f8a2b6ff33afe150fa1aeecfc938
jrovny <jrovny@JROVNYDELL7540>
2020-11-12 23:03:22 -0600

    Add UseProxyToSpaDevelopmentServer

--- src/TorchFireFilms/Startup.cs
+++ src/TorchFireFilms/Startup.cs
@@ -60,14 +60,11 @@ namespace TorchFireFilms
 
             app.UseSpa(spa =>
             {
-                // To learn more about options for serving an Angular SPA from ASP.NET Core,
-                // see https://go.microsoft.com/fwlink/?linkid=864501
-
                 spa.Options.SourcePath = "ClientApp";
 
                 if (env.IsDevelopment())
                 {
-                    spa.UseAngularCliServer(npmScript: "start");
+                    spa.UseProxyToSpaDevelopmentServer("http://localhost:4200");
                 }
             });
         }
