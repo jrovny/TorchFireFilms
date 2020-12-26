5d63832337a0e8f7993c7c2be102b3e7ad8ed41e
jrovny <jrovny@JROVNYDELL7540>
2020-12-25 21:19:26 -0600

    Add more endpoints for values controller

--- src/TorchFireFilms/Controllers/ValuesController.cs
+++ src/TorchFireFilms/Controllers/ValuesController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using TorchFireFilms.Api.Values;
 
@@ -20,6 +21,28 @@ namespace TorchFireFilms.Controllers
         public async Task<IEnumerable<Value>> GetAllAsync()
         {
             return await _context.Values.AsNoTracking().ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetByIdAsync(int id)
+        {
+            return Ok(await _context.Values.AsNoTracking().Where(v => v.ValueId == id).FirstOrDefaultAsync());
+        }
+
+        [HttpPost()]
+        public async Task<IActionResult> CreateAsync(Value value)
+        {
+            _context.Values.Add(value);
+            await _context.SaveChangesAsync();
+            return CreatedAtAction(nameof(GetByIdAsync), new { id = value.ValueId }, value);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateAsync(Value value)
+        {
+            _context.Values.Update(value);
+            await _context.SaveChangesAsync();
+            return Ok(value);
         }
     }
 }
