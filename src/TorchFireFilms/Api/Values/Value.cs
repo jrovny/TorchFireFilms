863d3efd2410b6ac64adb4bb53315dbfc3254815
jrovny <jrovny@JROVNYDELL7540>
2020-12-25 21:08:13 -0600

    Add Values controller for testing

---
+++ src/TorchFireFilms/Api/Values/Value.cs
@@ -0,0 +1,9 @@
+﻿namespace TorchFireFilms.Api.Values
+{
+    public class Value
+    {
+        public int ValueId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
---
+++ src/TorchFireFilms/Controllers/ValuesController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using TorchFireFilms.Api.Values;
+
+namespace TorchFireFilms.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ValuesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Value>> GetAllAsync()
+        {
+            return await _context.Values.AsNoTracking().ToListAsync();
+        }
+    }
+}
