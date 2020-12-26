615ac1da82f988748713b3010f9212805fd3ca57
jrovny <jrovny@JROVNYDELL7540>
2020-12-25 21:03:35 -0600

    Add ApplicationDbContext class

---
+++ src/TorchFireFilms/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TorchFireFilms.Api.Values;
+
+namespace TorchFireFilms
+{
+    public class ApplicationDbContext : DbContext
+    {
+        private readonly IConnectionService _connectionService;
+
+        public DbSet<Value> Values { get; set; }
+
+        public ApplicationDbContext(IConnectionService connectionService)
+        {
+            _connectionService = connectionService;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder builder)
+        {
+            builder.UseNpgsql(_connectionService.GetDefaultConnectionString());
+        }
+    }
+}
