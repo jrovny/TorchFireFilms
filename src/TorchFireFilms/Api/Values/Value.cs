b7e5d7abd9ed180d0e515a87d2651fbff36c4326
jrovny <jrovny@JROVNYDELL7540>
2020-12-25 21:19:15 -0600

    Add table annotations for value class

--- src/TorchFireFilms/Api/Values/Value.cs
+++ src/TorchFireFilms/Api/Values/Value.cs
@@ -1,9 +1,15 @@
-﻿namespace TorchFireFilms.Api.Values
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TorchFireFilms.Api.Values
 {
+    [Table("value")]
     public class Value
     {
+        [Column("value_id")]
         public int ValueId { get; set; }
+        [Column("name")]
         public string Name { get; set; }
+        [Column("description")]
         public string Description { get; set; }
     }
 }
