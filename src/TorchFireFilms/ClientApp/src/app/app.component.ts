e7dcc591dcc5b1138130b7e85f5e9491ec4740af
jrovny <jrovny@JROVNYDELL7540>
2020-12-26 18:53:21 -0600

    Add embedded Vimdeo videos for testing

--- src/TorchFireFilms/ClientApp/src/app/app.component.ts
+++ src/TorchFireFilms/ClientApp/src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
+import * as Player from '@vimeo/player/dist/player.js';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'angular-sample-app';
+  player: Player;
+
+  constructor() {
+    this.player = new Player();
+  }
 }
