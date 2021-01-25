3b32760308c703a0bd83013c8c5aff5475a168a7
jrovny <jrovny@JROVNYDELL7540>
2021-01-24 22:52:24 -0600

    Add baseUrl property to env files

--- src/TorchFireFilms/ClientApp/src/environments/environment.staging.ts
+++ src/TorchFireFilms/ClientApp/src/environments/environment.staging.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: false,
+  baseUrl: 'https://test.torchfirefilms.com',
   auth: {
     authority: 'https://test.accounts.torchfirefilms.com',
     redirectUri: 'https://test.torchfirefilms.com/signin-redirect',
--- src/TorchFireFilms/ClientApp/src/environments/environment.ts
+++ src/TorchFireFilms/ClientApp/src/environments/environment.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: false,
+  baseUrl: 'https://localhost:5010',
   auth: {
     authority: 'https://test.accounts.torchfirefilms.com',
     redirectUri: 'https://localhost:5010/signin-redirect',
