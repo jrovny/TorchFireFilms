c5f48bf267cbd32246533ccf82c7742aa103f1c1
jrovny <jrovny@JROVNYDELL7540>
2021-01-26 00:23:49 -0600

    Add slug property to film

--- src/TorchFireFilms/Api/Films/Data/Models/Film.cs
+++ src/TorchFireFilms/Api/Films/Data/Models/Film.cs
@@ -19,6 +19,8 @@ namespace TorchFireFilms.Api.Films.Data.Models
         public Uri Image { get; set; }
         [Column("thumbnail")]
         public Uri Thumbnail { get; set; }
+        [Column("slug")]
+        public string Slug { get; set; }
         [Column("created_date")]
         public DateTime CreatedDate { get; set; }
         [Column("created_user_id")]
