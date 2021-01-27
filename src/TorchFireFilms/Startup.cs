d764b6d3f97a7a08b208bf6736504d3a2e369b47
jrovny <jrovny@JROVNYDELL7540>
2021-01-26 22:28:03 -0600

    Remove unused variables and comments

--- src/TorchFireFilms/Startup.cs
+++ src/TorchFireFilms/Startup.cs
@@ -1,12 +1,9 @@
 using AutoMapper;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.SpaServices.AngularCli;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.IdentityModel.Tokens;
 using Serilog;
 
 namespace TorchFireFilms
@@ -20,7 +17,6 @@ namespace TorchFireFilms
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<ApplicationDbContext>();
@@ -28,8 +24,6 @@ namespace TorchFireFilms
             services.AddAutoMapper(typeof(Startup));
             services.AddControllersWithViews(options =>
                 options.SuppressAsyncSuffixInActionNames = false);
-            var authority = Configuration["AuthSettings:Authority"];
-            var audience = Configuration["AuthSettings:Audience"];
 
             services.AddAuthentication("Bearer")
                 .AddJwtBearer("Bearer", options =>
@@ -44,14 +38,12 @@ namespace TorchFireFilms
                     builder.WithOrigins("https://test.accounts.torchfirefilms.com");
                 });
             });
-            // In production, the Angular files will be served from this directory
             services.AddSpaStaticFiles(configuration =>
             {
                 configuration.RootPath = "dist";
             });
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
@@ -61,7 +53,6 @@ namespace TorchFireFilms
             else
             {
                 app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
 
