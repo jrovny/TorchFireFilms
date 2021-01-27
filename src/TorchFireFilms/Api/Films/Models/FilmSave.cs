a0fe2e0fcc33a0866353f91c8d01a08b5035cd3b
jrovny <jrovny@JROVNYDELL7540>
2021-01-26 22:41:50 -0600

    Add data annotations to film save model

--- src/TorchFireFilms/Api/Films/Models/FilmSave.cs
+++ src/TorchFireFilms/Api/Films/Models/FilmSave.cs
@@ -1,10 +1,12 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace TorchFireFilms.Api.Films.Models
 {
     public class FilmSave
     {
         public int FilmId { get; set; }
+        [Required]
         public string Title { get; set; }
         public string Description { get; set; }
         public int RuntimeMinutes { get; set; }
@@ -13,6 +15,7 @@ namespace TorchFireFilms.Api.Films.Models
         public Uri Image { get; set; }
         public Uri Thumbnail { get; set; }
         public string Slug { get; set; }
+        [Range(1, int.MaxValue)]
         public int IsoLanguage639Id { get; set; }
     }
 }
