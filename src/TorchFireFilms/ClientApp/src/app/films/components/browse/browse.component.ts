7b5424ae861edf797899e62fa6b914826024757e
jrovny <jrovny@JROVNYDELL7540>
2021-02-10 20:24:39 -0600

    Add detail to browse component

--- src/TorchFireFilms/ClientApp/src/app/films/components/browse/browse.component.ts
+++ src/TorchFireFilms/ClientApp/src/app/films/components/browse/browse.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { Observable } from 'rxjs';
 import { FilmBrowse } from '../../film-browse';
 import { FilmsService } from '../../films.service';
@@ -7,13 +7,22 @@ import { FilmsService } from '../../films.service';
   selector: 'app-browse',
   templateUrl: './browse.component.html',
   styleUrls: ['./browse.component.scss'],
+  encapsulation: ViewEncapsulation.None,
 })
 export class BrowseComponent implements OnInit {
   films$: Observable<FilmBrowse[]>;
+  film$: Observable<FilmBrowse>;
 
   constructor(private filmsService: FilmsService) {}
 
   ngOnInit(): void {
     this.films$ = this.filmsService.getFilms();
+
+    // this.films$.subscribe((film) => console.log(film));
+    // this.filmsService.getFilmById().subscribe((film) => console.log(film));
+  }
+
+  openFilm() {
+    console.log('Opening film');
   }
 }
